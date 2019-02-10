10e25df386c084e0237bdfef131ae88863978aa3
Brian Birir <brianbirir@gmail.com>
2019-02-10 22:40:33 +0300

    Add LCD display

--- dht11_arduino.ino
+++ dht11_arduino.ino
@@ -1,21 +1,27 @@
 #include <dht.h>
+#include <LiquidCrystal.h>
 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 dht DHT;
 
 #define DHT11_PIN 7
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-
+  lcd.begin(16, 2);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int chk = DHT.read11(DHT11_PIN);
-  Serial.print("Temperature = ");
-  Serial.println(DHT.temperature);
-  Serial.print("Humidity = ");
-  Serial.println(DHT.humidity);
-  delay(2000);
+  lcd.setCursor(0,0); 
+  lcd.print("Temp: ");
+  lcd.print(DHT.temperature);
+  lcd.print((char)223);
+  lcd.print("C");
+  lcd.setCursor(0,1);
+  lcd.print("Humidity: ");
+  lcd.print(DHT.humidity);
+  lcd.print("%");
+  delay(4000);
 }
